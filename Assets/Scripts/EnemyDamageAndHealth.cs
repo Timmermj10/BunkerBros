a98d6dc948c948cebf3f70613ecb3400632806f9
jroszko <jroszko@umich.edu>
2024-03-12 22:19:55 -0400

    Enemy Movement and Health

---
+++ Assets/Scripts/EnemyDamageAndHealth.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamageAndHealth : MonoBehaviour
+{
+
+    public int health = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Touched " + collision.collider.tag);
+        if (collision.collider.tag == "Player")
+        {
+            //Deal damage
+            DealDamage(collision);
+            
+        }
+        else if (collision.collider.tag == "Objective")
+        {
+            //Deal damage
+            DealDamage(collision);
+        }
+        else if (collision.collider.tag == "Structure")
+        {
+            //Deal damage
+            DealDamage(collision);
+        } //else if sword or weapon? or have them call takedamage? < call is probably easier
+    }
+
+    public void DealDamage(Collision c)
+    {
+        //Decrement health of player, structure, or objective > probably in has health component
+    }
+
+    public void TakeDamage(int damage)
+    {
+        //Take damage
+        health -= damage;
+        if (health <= 0)
+        {
+            EnemyDefeated();
+        }
+    }
+
+    private void EnemyDefeated()
+    {
+        Vector3 loca = transform.position;
+        //For Spawning resource pickup, and maybe spawner/respawn
+        EventBus.Publish<EnemyDefeat>(new EnemyDefeat(loca));
+
+        //Delete or reset
+        //Defaulting to Delete for now
+        GameObject.Destroy(gameObject);
+    }
+}
+
+public class EnemyDefeat
+{
+    Vector2 spawn_location;
+
+    public EnemyDefeat(Vector3 location)
+    {
+        spawn_location = location;
+    }
+}
---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    
+    public float speed = 5f;
+    private GameObject obj;
+    private Vector3 obj_loca;
+    private bool detect = false;
+
+    
+    void Awake()
+    {
+        obj = GameObject.Find("Objective");
+        obj_loca = obj.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Time.deltaTime;
+        move();
+    }
+
+    private void move()
+    {
+        Vector3 play = obj_loca - transform.position;
+        Vector3 newLoca = play.normalized * speed * Time.deltaTime;
+        newLoca.y = 0;
+        
+        transform.position += newLoca;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Touched " + collision.collider.tag);
+        if (collision.collider.tag == "Player")
+        {
+            //Deal damage
+        } else if (collision.collider.tag == "Objective")
+        {
+            //Deal damage
+        } else if (collision.collider.tag == "Structure")
+        {
+            //Deal damage
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && (detect == false))
+        {
+            obj = other.gameObject;
+            obj_loca = obj.transform.position;
+            detect = true;
+            Debug.Log("Detected");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            detect = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            obj = GameObject.Find("Objective");
+            obj_loca = obj.transform.position;
+            detect = false;
+        }
+    }
+
+}
