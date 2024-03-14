3e64999255221b466f3fd8d1d7398cece09093fa
jroszko <jroszko@umich.edu>
2024-03-13 23:41:09 -0400

    Enemy Spawner v1

---
+++ Assets/Scripts/EnemySpawnManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnManager : MonoBehaviour
+{
+
+    public GameObject EnemyPrefab;
+    public float spawnDelay = 200f;
+    public float random_spawn = 5f;
+    private float random;
+    private float initDelay;
+
+    private void Start()
+    {
+        initDelay = spawnDelay;
+    }
+
+    void FixedUpdate()
+    {
+        random = Random.Range(-random_spawn, random_spawn);
+        if (spawnDelay <= 0)
+        {
+            Instantiate(EnemyPrefab);
+            Vector3 loca = transform.position;
+            loca.x += random;
+            EnemyPrefab.transform.position = loca;
+            spawnDelay = initDelay;
+        } else
+        {
+            spawnDelay -= 1;
+        }
+    }
+}
