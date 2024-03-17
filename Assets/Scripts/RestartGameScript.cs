b0cac99dbe8e93006778ef7f27931b049cceeccc
Jacob Timmerman <timmermj@umich.edu>
2024-03-17 17:21:24 -0400

    GameEndScene

---
+++ Assets/Scripts/RestartGameScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGameScript : MonoBehaviour
+{
+    public void OnGameRestart()
+    {
+        SceneManager.LoadSceneAsync("Main");
+    }
+}
