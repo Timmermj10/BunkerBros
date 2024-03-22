10bea425afd0b0ae4c7d2b5291cf00c11c583f9e
Jacob Timmerman <timmermj@umich.edu>
2024-03-22 17:31:52 -0400

    Tower and Player health bars

--- Assets/Scripts/HasHealth.cs
+++ Assets/Scripts/HasHealth.cs
@@ -8,14 +8,32 @@ public class HasHealth : MonoBehaviour
     public int maxHealth = 1;
     public int currentHealth;
 
+    public HealthBarScript healthBar;
+
     private void Start()
     {
+        // Set the current health to the max possible
         currentHealth = maxHealth;
+
+        // Set the health bar to max health
+        if (healthBar != null )
+        {
+            healthBar.SetMaxHealth(maxHealth);
+        }
     }
 
     public void changeHealth(int healthChange)
     {
+        // Update currentHealth
         currentHealth += healthChange;
+
+        // Update slider
+        if (healthBar != null )
+        {
+            healthBar.SetHealth(currentHealth);
+        }
+
+        // If we have less than 0 health, DIE
         if (currentHealth <= 0)
         {
             Die();
