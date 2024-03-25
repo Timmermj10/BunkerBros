855e8052f7d9164a8e81257a5e0ba64d488405e9
Jacob Timmerman <timmermj@umich.edu>
2024-03-25 00:59:45 -0400

    Prompts for collectables

---
+++ Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour
+{
+    [SerializeField]
+    private Text promptText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void UpdateText(string promptMessage)
+    {
+        promptText.text = promptMessage;
+    }
+}
