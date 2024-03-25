c753e3c1e951b600591d14b9c9175b3a65e1d797
Jacob Timmerman <timmermj@umich.edu>
2024-03-24 23:01:13 -0400

    Start new wave button

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WaveManager : MonoBehaviour
 {
-
     private int waveNumber = 1;
     private int maxEnemiesAtOnce = 24;
     private int currentEnemiesAlive = 0;
     private int numEnemiesToSpawnThisRound = 5;
     private int numEnemiesSpawnedSoFar = 0;
 
+    [Header("Wave Button")]
+    public GameObject waveButton;
+
     private void Start()
     {
         EventBus.Subscribe<ObjectDestroyedEvent>(_DidEnemyDie);
+        EventBus.Subscribe<WaveEndedEvent>(_WaveEnd);
 
 
         //Just for testing, immediately start the wave
@@ -77,9 +81,18 @@ public class WaveManager : MonoBehaviour
         }
     }
 
+    public void StartWave()
+    {
+        // Publish the start wave event
+        EventBus.Publish(new WaveStartedEvent());
 
+        // Make the button not interactable
+        waveButton.GetComponent<Button>().interactable = false;
+    }
 
-
-
-
+    public void _WaveEnd(WaveEndedEvent e)
+    {
+        // Make the button interactable
+        waveButton.GetComponent<Button>().interactable = true;
+    }
 }
