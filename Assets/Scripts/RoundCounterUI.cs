2c43fb41030c32793844db260fb754ebf44fa376
Jacob Timmerman <timmermj@umich.edu>
2024-03-27 18:03:42 -0400

    Round Counter

---
+++ Assets/Scripts/RoundCounterUI.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoundCounterUI : MonoBehaviour
+{
+    // Round count
+    private int roundCount = 0;
+
+    // Manager Round Text
+    private Text managerRounds;
+
+    // Player Round Text
+    private Text playerRounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Subscribe to round start events
+        EventBus.Subscribe<WaveStartedEvent>(_UpdateRoundUI);
+
+        // Get reference to the text for rounds
+        managerRounds = GameObject.Find("ManagerRoundCounter").GetComponent<Text>();
+        playerRounds = GameObject.Find("PlayerRoundCounter").GetComponent<Text>();
+    }
+
+    public void _UpdateRoundUI(WaveStartedEvent e)
+    {
+        // Increment the round count
+        roundCount++;
+
+        // Update the UI
+        managerRounds.text = $"Round: {roundCount}";
+        playerRounds.text = $"Round: {roundCount}";
+    }
+}
