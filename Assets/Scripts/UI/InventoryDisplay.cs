e0596810ff4397f0c3fc08fb5c4ef0483696150a
jroszko <jroszko@umich.edu>
2024-04-03 00:05:05 -0400

    UI

---
+++ Assets/Scripts/UI/InventoryDisplay.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryDisplay : MonoBehaviour
+{
+    public ActivePlayerInventory.activePlayerItems item_to_count;
+    public Text count;
+    public int num = 0;
+    public ActivePlayerInventory inv;
+    private Image img;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        num = inv.countItem(item_to_count);
+        if (num <= 0)
+        {
+            count.text = "";
+            img.color = new Color(img.color.r, img.color.g, img.color.b, 0f);
+        } else
+        {
+            img.color = new Color(img.color.r, img.color.g, img.color.b, 1f);
+            count.text = num.ToString();
+        }
+
+    }
+}
