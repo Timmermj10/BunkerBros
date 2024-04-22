ce6cf59d657f9277fb23c74b80b2c045377c73c4
Dexter <dexken@umich.edu>
2024-04-21 20:18:59 -0400

    removed PS from DoNotDestroyOnLoad

--- Assets/Scripts/Managers/ParticleManager.cs
+++ Assets/Scripts/Managers/ParticleManager.cs
@@ -14,7 +14,6 @@ public class ParticleManager : MonoBehaviour
         if(instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
